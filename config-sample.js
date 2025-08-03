60b2125273ac8d8d319c4b6c0a34e318338aee9c
Syuhadak27 <117827583+Syuhadak27@users.noreply.github.com>
2025-08-04 06:46:20 +0700

    Update config-sample.js

--- config-sample.js
+++ config-sample.js
@@ -1,4 +1,10 @@
 export const config = {
   GOOGLE_API_KEY: "",
-  SPREADSHEET_ID: ""
+  BOT_TOKEN: "",
+  CHANNEL_ID: "-100xxx",
+  KIRIM_LOG: "telegram",
+  SPREADSHEET_ID: "",
+  USER_WEB: ["user1", "user2", "user3"],
+  ADMIN_USERNAME: "admin",
+  ADMIN_PASWORD: "admin"
 };
